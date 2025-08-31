00547b6468fd134cf5a6597bd2d26303237c57aa
unauthorised-401 <youssefshahean0@gmail.com>
2025-08-31 19:17:58 +0300

    facade and proxy

---
+++ LoggingProxy.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'common/FacadeInterface.php';
+require_once 'common/DatabaseLogger.php';
+require_once 'facade.php';
+
+class LoggingProxy implements FacadeInterface
+{
+    private $facade;
+    private $logger;
+
+    public function __construct()
+    {
+        $this->facade = new CharityFacade();
+        $this->logger = new DatabaseLogger();
+    }
+
+    public function handleRequest($action, $method, $data = [])
+    {
+        $this->logger->log($action, $method);
+
+        $this->facade->handleRequest($action, $method, $data);
+    }
+}
