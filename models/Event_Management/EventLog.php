96e26459e2e8f7150342850ccb61696b4540c957
Abdallah Gabr <132190312+AbdallahGabr11@users.noreply.github.com>
2025-08-31 19:52:12 +0300

    Update EventLog.php

--- models/Event_Management/EventLog.php
+++ models/Event_Management/EventLog.php
@@ -1,18 +1,15 @@
 <?php
 require_once(__DIR__ . '/../../config/Database.php');
 
-class EventLog
-{
+class EventLog {
     private $conn;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->conn = Database::getInstance()->getConnection();
     }
 
     // Add a log entry
-    public function addLog($eventId, $action, $payload = null)
-    {
+    public function addLog($eventId, $action, $payload = null) {
         $stmt = $this->conn->prepare("
             INSERT INTO event_logs (event_id, action, payload)
             VALUES (?, ?, ?)
@@ -22,8 +19,7 @@ class EventLog
     }
 
     // Get logs for an event
-    public function getByEvent($eventId)
-    {
+    public function getByEvent($eventId) {
         $stmt = $this->conn->prepare("SELECT * FROM event_logs WHERE event_id = ? ORDER BY created_at DESC");
         $stmt->bind_param("i", $eventId);
         $stmt->execute();
