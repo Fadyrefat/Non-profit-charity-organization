08fd3f4054db947fd37544dce5b26035752cccdd
Abdallah Gabr <132190312+AbdallahGabr11@users.noreply.github.com>
2025-09-01 16:07:29 +0300

    Update BookTicketCommand.php

--- models/Event_Management/Command/BookTicketCommand.php
+++ models/Event_Management/Command/BookTicketCommand.php
@@ -1,17 +1,13 @@
 <?php
 require_once __DIR__ . '/EventCommandInterface.php';
-require_once __DIR__ . '/../Attendee.php';
-require_once __DIR__ . '/../EventLog.php';
 
 class BookTicketCommand implements EventCommandInterface {
     private $receiver;
-    private $event;
     private $eventId;
     private $data;
 
-    public function __construct($receiver, $event, $eventId, $data) {
+    public function __construct($receiver, $eventId, $data) {
         $this->receiver = $receiver;
-        $this->event    = $event;
         $this->eventId  = $eventId;
         $this->data     = $data;
     }
@@ -22,16 +18,6 @@ class BookTicketCommand implements EventCommandInterface {
             ? $this->data['ticket_type']
             : 'General';
 
-        $db = Database::getInstance()->getConnection();
-        $stmt = $db->prepare("UPDATE attendees SET ticket_type = ? WHERE id = ?");
-        $stmt->bind_param("si", $ticketType, $attendeeId);
-        $stmt->execute();
-
-        // Log with event_id context
-        $log = new EventLog();
-        $log->addLog($this->eventId, 'BookTicket', json_encode([
-            'attendee_id' => $attendeeId,
-            'ticket_type' => $ticketType
-        ]));
+        $this->receiver->bookTicket($this->eventId, $attendeeId, $ticketType);
     }
 }
